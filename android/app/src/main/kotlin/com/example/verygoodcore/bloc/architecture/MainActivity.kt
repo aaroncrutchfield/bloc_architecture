ebba59253b9ad3224b4f20fe458e20a615d6e2de
Aaron Crutchfield <aaroncrutchfield09@gmail.com>
2025-06-17 19:01:02 -0400

    first commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/bloc/architecture/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.bloc_architecture
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_development.dart
@@ -0,0 +1,6 @@
+import 'package:bloc_architecture/app/app.dart';
+import 'package:bloc_architecture/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
